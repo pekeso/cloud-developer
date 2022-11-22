59b8c39c8b0643dd306e5b6169230b1d863c5497
Joel Mbiye <joembiye@gmail.com>
2022-11-22 08:24:14 +0100

    modified apiId

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'gt16pwnvnf'
+const apiId = 'o6mwdni6x0'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
